6c43806cd66c8f49680d2c2f9d827f7d7ecbea11
Billy Fletcher <Billyfletcher99@gmail.com>
2022-10-27 20:54:42 -0400

    Changed hello to hi to see if branch is working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-neutral-900 text-gray-300'>
     <h1 className="text-3xl font-bold underline text-yellow-100">
-      Hello world!
+      hiiiiiiii!
     </h1>
     </div>
   )
