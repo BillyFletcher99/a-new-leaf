3d7479cebe01c3e974a797695e43f451d207f5ce
Billy Fletcher <Billyfletcher99@gmail.com>
2022-10-27 21:42:10 -0400

    lets see if it works

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-neutral-900 text-gray-300'>
+    <h1 className="text-3xl font-bold underline text-yellow-100">
+      hiiiiiiii!
+    </h1>
+    </div>
+  )
+}
+
+export default NavBar
